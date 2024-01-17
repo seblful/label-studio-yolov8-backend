7d6a0bba3f1f23a0b2da95a35d5bde68aac17235
seblful <alesha.chimba@gmail.com>
2024-01-17 22:28:53 +0300

    [EDIT] fix bags, new ultralytics

--- model.py
+++ model.py
@@ -1,17 +1,24 @@
 import os
 import random
 import requests
+import socket
 from PIL import Image
 from io import BytesIO
 
+import numpy as np
 from ultralytics import YOLO
+
 from label_studio_ml.model import LabelStudioMLBase
 from label_studio_ml.utils import get_single_tag_keys, get_local_path
 
-# URL with host
-# LS_URL =  "http://127.0.0.1:8080"
+
+# hostname = socket.gethostname()
+# LS_URL = socket.gethostbyname(hostname)
+# print("Hostname: ", hostname)
+# print("IP Address: ", ip_address)
+
 LS_URL = "http://192.168.100.3:8080"
-LS_API_TOKEN = "your api token"
+LS_API_TOKEN = "201a66049e8438c587125d8163d2d60538f067d5"
 
 
 # Initialize class inhereted from LabelStudioMLBase
@@ -25,7 +32,7 @@ class YOLOv8Model(LabelStudioMLBase):
             self.parsed_label_config, 'PolygonLabels', 'Image')
         self.labels = ['capsules', 'tablets']
         # Load model
-        self.model = YOLO("best.pt")
+        self.model = YOLO("best_seg.pt")
 
     # Function to predict
     def predict(self, tasks, **kwargs):
@@ -42,33 +49,35 @@ class YOLOv8Model(LabelStudioMLBase):
         # Header to get request
         header = {
             "Authorization": "Token " + LS_API_TOKEN}
-        
+
         # Getting URL and loading image
         image = Image.open(BytesIO(requests.get(
             full_url, headers=header).content))
         # Height and width of image
         original_width, original_height = image.size
-        
+
         # Creating list for predictions and variable for scores
         predictions = []
         score = 0
-        
+        i = 0
 
         # Getting prediction using model
         results = self.model.predict(image)
-        
 
         # Getting mask segments, boxes from model prediction
         for result in results:
-            for i, (box, segm) in enumerate(zip(result.boxes, result.masks.segments)):
-                
-                # 2D array with poligon points 
-                polygon_points = (segm * 100).tolist()
+            for i, (box, segm) in enumerate(zip(result.boxes, result.masks.xy)):
+
+                # 2D array with poligon points
+                polygon_points = segm / \
+                    np.array([original_width, original_height]) * 100
+
+                polygon_points = polygon_points.tolist()
 
                 # Adding dict to prediction
                 predictions.append({
-                    "from_name" : self.from_name,
-                    "to_name" : self.to_name,
+                    "from_name": self.from_name,
+                    "to_name": self.to_name,
                     "id": str(i),
                     "type": "polygonlabels",
                     "score": box.conf.item(),
@@ -83,8 +92,7 @@ class YOLOv8Model(LabelStudioMLBase):
                 # Calculating score
                 score += box.conf.item()
 
-
-        print(10*"#", "Returned Prediction", 10*"#")
+        print(f"Prediction Score is {score:.3f}.")
 
         # Dict with final dicts with predictions
         final_prediction = [{
@@ -94,7 +102,7 @@ class YOLOv8Model(LabelStudioMLBase):
         }]
 
         return final_prediction
-    
+
     def fit(self, completions, workdir=None, **kwargs):
         """ 
         Dummy function to train model
